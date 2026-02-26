d3d285e51c1e494920e8e463a11f2bea9b3beb2c
Japan <106416229+ElSaanti@users.noreply.github.com>
2026-02-26 07:16:05 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,16 @@
 import streamlit as st
+from PIL import Image
 
 st.title("MI PRIMER PAGINA CON STREAMLIT")
+
+st.header("En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales.")
+st.write("Facilmente puedo realizar backend y frontend.")
+Image = Image.open("Interfaces Mult2.png")
+
+st.image(image, caption= "Interfaces multimodales")
+
+
+texto = st.text_input("Escribe algo", "Este es mi texto")
+st.write("El texto escrito es", texto)
+
+st.subheader("Ahora usemos 2 Columnas")
